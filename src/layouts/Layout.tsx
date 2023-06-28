f01f33f0fee43084e9da6283fd4b1991038b05e7
pySoo <tngus97821@naver.com>
2023-06-28 23:14:07 +0900

    [feat] : 공통 layout 컴포넌트 생성

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,37 @@
+import { ReactNode, useMemo } from 'react';
+import LinkExternal from '@/common/LinkExternal';
+import { siteConfig } from '@/constants/config';
+import HeaderNav from './HeaderNav';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  const since = useMemo(() => {
+    const year = new Date().getFullYear();
+    if (siteConfig.since === year) {
+      return year;
+    }
+    return `${siteConfig.since}-${year}`;
+  }, []);
+
+  return (
+    <div className="mx-auto max-w-3xl px-6 lg:max-w-6xl lg:px-8">
+      <HeaderNav />
+      <main className="relative pb-16">{children}</main>
+      <footer className="pb-8 text-sm text-neutral-800 dark:text-neutral-400">
+        <div className="flex flex-col items-end space-y-1">
+          <p>
+            <span>© {since} </span>
+            <LinkExternal href={siteConfig.author.contacts.github}>
+              {siteConfig.title}
+            </LinkExternal>
+            <span> Powered by </span>
+            <LinkExternal href="https://nextjs.org/">Next.js</LinkExternal>
+            <span>, </span>
+            <LinkExternal href="https://pages.github.com/">
+              Github Pages
+            </LinkExternal>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}

98efcba487f821292b826a246e8a5ff8f0d722b4
pySoo <tngus97821@naver.com>
2023-06-28 23:15:24 +0900

    [feat] : 페이지별 SEO 설정 컴포넌트 추가

---
+++ src/components/SEO.tsx
@@ -0,0 +1,109 @@
+import { siteConfig } from '@/constants/config';
+import { ArticleJsonLd, NextSeo } from 'next-seo';
+
+const getTitle = (title?: string) => {
+  if (!title) return siteConfig.title;
+  if (title.length > 10) return title;
+
+  return `${title} - pySoo`;
+};
+
+const getRelativeUrl = (url?: string) => {
+  if (!url) return siteConfig.url;
+
+  return `${siteConfig.url}/${url.replace(/^\/+/g, '')}`;
+};
+
+const DEFAULT_IMAGE = `${siteConfig.url}/images/base.jpg`;
+
+const getImageUrl = (img?: string) => {
+  if (!img) return DEFAULT_IMAGE;
+  if (img.startsWith('https://')) return img;
+
+  return `${siteConfig.url}/${img}`;
+};
+
+/**
+ * use in Normal Page SEO
+ */
+export const PageSEO = (props: {
+  title?: string;
+  description?: string;
+  url?: string;
+  image?: string;
+}) => {
+  const title = getTitle(props.title);
+  const description = props.description ?? siteConfig.description;
+  const url = getRelativeUrl(props.url);
+  const image = getImageUrl(props.image);
+
+  return (
+    <NextSeo
+      title={title}
+      description={description}
+      canonical={url}
+      openGraph={{
+        url,
+        title,
+        description,
+        images: [{ url: image }],
+      }}
+    />
+  );
+};
+
+/**
+ * use in Article Page SEO
+ */
+export const BlogSEO = ({
+  summary,
+  tags,
+  images,
+  ...props
+}: {
+  title: string;
+  summary: string;
+  date: string;
+  url: string;
+  tags: string[];
+  images: string[];
+}) => {
+  const title = getTitle(props.title);
+  const url = getRelativeUrl(props.url);
+  const dateTime = new Date(props.date).toISOString();
+  const imageList = images.length ? images.map(getImageUrl) : [DEFAULT_IMAGE];
+
+  return (
+    <>
+      <NextSeo
+        title={title}
+        description={summary}
+        canonical={url}
+        openGraph={{
+          type: 'article',
+          article: {
+            publishedTime: dateTime,
+            modifiedTime: dateTime,
+            authors: [`https://github.com/pySoo`],
+            tags,
+          },
+          url,
+          title,
+          description: summary,
+          images: imageList.map((img) => ({ url: img })),
+        }}
+      />
+      <ArticleJsonLd
+        datePublished={dateTime}
+        dateModified={dateTime}
+        images={imageList}
+        url={url}
+        title={title}
+        description={summary}
+        authorName={siteConfig.author.name}
+        publisherName={siteConfig.author.name}
+        publisherLogo={`${siteConfig.url}/favicon.ico`}
+      />
+    </>
+  );
+};

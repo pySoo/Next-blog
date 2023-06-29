aa619af7aacd057c2a089fca67c04e1e21a0ef33
pySoo <soopy368@gmail.com>
2023-06-29 18:34:26 +0900

    [chore] : eslint에 unused import, simple import sort 기능 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
+import './globals.css';
+import { seoConfig } from '@/constants/config';
+import { $ } from '@/libs/core';
+import { DefaultSeo } from 'next-seo';
+import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <DefaultSeo {...seoConfig} />
+      <div className={$('font-sans')}>
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,75 @@
+import { motion } from 'framer-motion';
 import PlainText from '@/common/PlainText';
 import Title from '@/common/Title';
 import { PageSEO } from '@/components/SEO';
 import Layout from '@/layouts/Layout';
 import React from 'react';
+import { fadeIn, staggerHalf } from '@/constants/animations';
+import SubTitle from '@/common/SubTitle';
+import LinkArrow from '@/common/LinkArrow';
+import { unsplashImageList } from '@/constants/image';
+import PostCard from '@/components/PostCard';
 
 export default function Home() {
   return (
     <Layout>
-      <PageSEO title="Blog" description="블로그 설명입니다." url="/blog" />
-      <Title>pySoo</Title>
-      <PlainText>환영합니다.</PlainText>
+      <PageSEO title="pySoo blog" description="블로그 설명입니다." url="/" />
+      <motion.section
+        variants={staggerHalf}
+        initial="initial"
+        animate="animate"
+      >
+        <Title>pySoo</Title>
+        <PlainText>
+          안녕하세요. 꾸준함과 글쓰기를 좋아하는 개발자 박수현입니다.
+        </PlainText>
+      </motion.section>
+      <motion.section
+        className="mt-16"
+        variants={staggerHalf}
+        initial="initial"
+        animate="animate"
+      >
+        <motion.div variants={fadeIn}>
+          <SubTitle className="mb-6">Featured Posts</SubTitle>
+        </motion.div>
+
+        <motion.div
+          variants={staggerHalf}
+          className="grid gap-6 md:grid-cols-4"
+        >
+          <PostCard
+            href="/blog/2023/06/test"
+            imgUrl={unsplashImageList[13]}
+            title="안녕하세요"
+            date="2023.06.29"
+          />
+          <PostCard
+            href="/blog/2023/06/test"
+            imgUrl={unsplashImageList[10]}
+            title="박수현 블로그입니다."
+            date="2023.06.29"
+          />
+          <PostCard
+            href="/blog/2023/06/test"
+            imgUrl={unsplashImageList[8]}
+            title="동해물과 백두산이 마르고 닳도록"
+            date="2023.06.29"
+          />
+          <PostCard
+            href="/blog/2023/06/test"
+            imgUrl={unsplashImageList[9]}
+            title="동해물과 백두산이 마르고 닳도록"
+            date="2023.06.29"
+          />
+        </motion.div>
+
+        <motion.div variants={fadeIn}>
+          <LinkArrow href="/blog" className="mt-8">
+            모든 글 보기
+          </LinkArrow>
+        </motion.div>
+      </motion.section>
     </Layout>
   );
 }

678c92230cf8db2ad499b8c272c19692354bfa91
pySoo <soopy368@gmail.com>
2023-06-30 19:40:12 +0900

    [feat] : contentlayer 설정 추가

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const { withContentlayer } = require('next-contentlayer');
+
+/** @type {import('next').NextConfig} */
+module.exports = withContentlayer({
+  reactStrictMode: true,
+  swcMinify: false,
+});

094f6d6c09d65b0554ef29e36dc1a7ae8a593429
pySoo <soopy368@gmail.com>
2023-07-01 19:31:21 +0900

    [feat] : 모든 시리즈 이름, 모든 시리즈 글을 불러오는 lib 추가

--- src/libs/dataset.ts
+++ src/libs/dataset.ts
@@ -1,15 +1,27 @@
 import { allPosts } from 'contentlayer/generated';
 
-import { Post } from '@/types/post';
+import { Post, Series } from '@/types/post';
 
 import { reducePost } from './post';
 
-export const allBlogPosts: Post[] = allPosts.filter(
-  (post) =>
-    !post.draft &&
-    post._raw.sourceFilePath.includes('blog') &&
-    !post._raw.sourceFilePath.includes('/index.mdx'),
-);
+export const allSeriesName = allPosts
+  .filter((post) => post._raw.sourceFilePath.includes('/index.mdx'))
+  .map((post) => post.slug.split('/')[2]);
+
+export const allBlogPosts: Post[] = allPosts
+  .filter(
+    (post) =>
+      !post.draft &&
+      post._raw.sourceFilePath.includes('blog') &&
+      !post._raw.sourceFilePath.includes('/index.mdx'),
+  )
+  .map((post) => ({
+    ...post,
+    seriesName:
+      allSeriesName.find((seriesName) => post.slug.includes(seriesName)) ??
+      null,
+  }))
+  .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
 export const allSnippets: Post[] = allPosts
   .filter((post) => post._raw.sourceFilePath.includes('snippets'))
@@ -20,3 +32,15 @@ export const allSnippets: Post[] = allPosts
   .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
 export const reducedAllSnippets = allSnippets.map(reducePost);
+
+export const allSeries: Series[] = allPosts
+  .filter((post) => post._raw.sourceFilePath.includes('/index.mdx'))
+  .map((series) => ({
+    ...series,
+    seriesName: series.slug.split('/')[2],
+    posts: allBlogPosts
+      .filter((post) => series.slug.includes(post.seriesName ?? 'none'))
+      .map(reducePost)
+      .reverse(),
+  }))
+  .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());

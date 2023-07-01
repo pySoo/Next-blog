c7ea56c0e711f91e384ab1d53de3b8c9c94431cb
pySoo <soopy368@gmail.com>
2023-07-01 17:07:40 +0900

    [feat] : snippet 파일 reduced 함수 추가

--- src/libs/dataset.ts
+++ src/libs/dataset.ts
@@ -2,9 +2,21 @@ import { allPosts } from 'contentlayer/generated';
 
 import { Post } from '@/types/post';
 
+import { reducePost } from './post';
+
 export const allBlogPosts: Post[] = allPosts.filter(
   (post) =>
     !post.draft &&
     post._raw.sourceFilePath.includes('blog') &&
     !post._raw.sourceFilePath.includes('/index.mdx'),
 );
+
+export const allSnippets: Post[] = allPosts
+  .filter((post) => post._raw.sourceFilePath.includes('snippets'))
+  .map((snippet) => ({
+    ...snippet,
+    snippetName: snippet.slug.split('/').at(2) ?? null,
+  }))
+  .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+export const reducedAllSnippets = allSnippets.map(reducePost);

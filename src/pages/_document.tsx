398c71cacd907a7b6660e53eb26913c10a88d672
pySoo <soopy368@gmail.com>
2023-07-03 18:54:04 +0900

    [style] : 전체 파일에 text, bg primary color 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="text-primary bg-primary">
         <Main />
         <NextScript />
       </body>

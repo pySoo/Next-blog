1cc0c841d827e3d83dbbc956dca4b1e6a788a1e7
pySoo <soopy368@gmail.com>
2023-07-03 18:55:09 +0900

    [style] : Pill 컴포넌트 글자 굵기 통일

--- src/common/Pill.tsx
+++ src/common/Pill.tsx
@@ -13,9 +13,7 @@ export default function Pill({ className, selected, ...props }: PillProps) {
       className={$(
         'rounded-lg px-2 py-0.5 transition-colors',
         'bg-secondary hover:text-primary hover:bg-tertiary',
-        selected
-          ? 'text-primary font-semibold ring-2 ring-neutral-350'
-          : 'text-secondary font-normal',
+        selected ? 'text-primary ring-2 ring-neutral-350' : 'text-secondary',
         className,
       )}
     />

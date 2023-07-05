53618f3733637728f23c390fc40b8bbeb3d6ce40
Suhyeon Park <soopy368@gmail.com>
2023-07-05 17:48:38 +0900

    [feat] : useDebounce hook 추가

---
+++ src/libs/useDebounce.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+const useDebounce = <T>(value: T, delay = 500) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;

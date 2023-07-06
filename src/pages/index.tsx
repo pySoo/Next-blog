8f4f1855d86d1326a6960b2925733b4ae002e9f5
Suhyeon Park <soopy368@gmail.com>
2023-07-06 23:40:37 +0900

    [feat] : isDraft 속성 추가 및 블로그 소개글 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 import React from 'react';
 
 import LinkArrow from '@/common/LinkArrow';
+import Pill from '@/common/Pill';
 import PlainText from '@/common/PlainText';
 import SubTitle from '@/common/SubTitle';
 import Title from '@/common/Title';
 import PostCard from '@/components/PostCard';
 import { PageSEO } from '@/components/SEO';
 import { fadeIn, staggerHalf } from '@/constants/animations';
-import { unsplashImageList } from '@/constants/image';
 import Layout from '@/layouts/Layout';
 
 export default function Home() {
@@ -20,13 +21,21 @@ export default function Home() {
         initial="initial"
         animate="animate"
       >
-        <Title>pySoo</Title>
+        <Title>Seize the day!</Title>
         <PlainText>
-          안녕하세요. 꾸준함과 글쓰기를 좋아하는 개발자 박수현입니다.
+          반갑습니다🤗 꾸준함과 글쓰기를 좋아하는 개발자 박수현입니다. <br />
+          현재 블로그 이사 작업을 하고 있습니다. 🚛 💭 💭 <br />
         </PlainText>
+        <div className="flex items-center text-tertiary">
+          더 많은 글을 읽어보시려면
+          <Link href={`https://velog.io/@soopy368`}>
+            <Pill className="w-fit mx-1">Velog</Pill>
+          </Link>
+          를 방문해주세요!
+        </div>
       </motion.section>
       <motion.section
-        className="mt-16"
+        className="mt-12"
         variants={staggerHalf}
         initial="initial"
         animate="animate"
@@ -40,28 +49,30 @@ export default function Home() {
           className="grid gap-6 md:grid-cols-4"
         >
           <PostCard
-            href="/blog/2023/06/test"
-            imgUrl={unsplashImageList[13]}
-            title="안녕하세요"
-            date="2023.06.29"
+            href="/blog/frontend/1-dom"
+            imgUrl="/posts/1-dom/cover.png"
+            title="웹을 이루는 핵심 요소를 알아봅시다! (DOM, BOM, JavaScript)"
+            date="2023.07.06"
           />
           <PostCard
-            href="/blog/2023/06/test"
-            imgUrl={unsplashImageList[10]}
-            title="박수현 블로그입니다."
-            date="2023.06.29"
+            href="/blog/javascript/1-var"
+            imgUrl="/posts/1-var/cover.png"
+            title="var 모르고 쓰면 큰일납니다."
+            date="2023.07.06"
           />
           <PostCard
-            href="/blog/2023/06/test"
-            imgUrl={unsplashImageList[8]}
-            title="동해물과 백두산이 마르고 닳도록"
-            date="2023.06.29"
+            href=""
+            imgUrl="/images/samples/draft-cover.png"
+            title="(공개 예정) Next.js의 SSR, SSG, ISR 이해하기 "
+            date="-"
+            isDraft
           />
           <PostCard
-            href="/blog/2023/06/test"
-            imgUrl={unsplashImageList[9]}
-            title="동해물과 백두산이 마르고 닳도록"
-            date="2023.06.29"
+            href=""
+            imgUrl="/images/samples/draft3.png"
+            title="(공개 예정) Critical Rendering Path"
+            date="-"
+            isDraft
           />
         </motion.div>
 

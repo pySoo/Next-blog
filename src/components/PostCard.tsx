3c1a4239c2d1be3b1c3af90b374b174c2bc651c8
Suhyeon Park <soopy368@gmail.com>
2023-07-07 04:25:58 +0900

    [feat] : 홈 화면 카드 fadeUp 애니메이션 추가

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -2,11 +2,11 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 
 import IconText from '@/common/IconText';
-import { fadeInUp } from '@/constants/animations';
+import { fadeIn, fadeInUp } from '@/constants/animations';
 
 import CalendarIcon from './icons/CalendarIcon';
 
-type PostCardType = {
+export type PostCardType = {
   href: string;
   imgUrl: string;
   title: string;
@@ -24,29 +24,45 @@ export default function PostCard({
   return (
     <motion.a
       href={isDraft ? undefined : href}
-      className={`overflow-hidden rounded-xl bg-neutral-200 dark:bg-neutral-800 ${
-        isDraft ? 'cursor-not-allowed' : ''
-      }`}
       variants={fadeInUp}
       whileHover={{ scale: isDraft ? 1 : 1.02 }}
       whileTap={{ scale: isDraft ? 1 : 0.98 }}
     >
-      <div className={`${isDraft ? 'opacity-50' : 'opacity-100'}`}>
-        <Image
-          src={imgUrl}
-          alt={title}
-          width={300}
-          height={300}
-          className={'h-64 w-full object-cover'}
-          draggable={false}
-        />
-        <div className="p-6">
-          <div className="mb-2 flex w-full items-end">
-            <IconText Icon={CalendarIcon} text={date} />
+      <motion.div
+        variants={fadeInUp}
+        className={`overflow-hidden rounded-xl bg-neutral-200 dark:bg-neutral-800 ${
+          isDraft ? 'cursor-not-allowed' : ''
+        }`}
+      >
+        <motion.div
+          variants={fadeIn}
+          initial="initial"
+          whileInView="animate"
+          exit="exit"
+          viewport={{ amount: 0.2, once: true }}
+        >
+          <div className={`${isDraft ? 'opacity-50' : 'opacity-100'}`}>
+            <Image
+              src={imgUrl}
+              alt={title}
+              width={300}
+              height={300}
+              className={'h-64 w-full object-cover'}
+              draggable={false}
+            />
+            <div className="p-6">
+              {date && (
+                <div className="mb-2 flex w-full items-end">
+                  <IconText Icon={CalendarIcon} text={date} />
+                </div>
+              )}
+              <p className="text-xl font-bold tracking-tight md:text-lg">
+                {title}
+              </p>
+            </div>
           </div>
-          <p className="text-xl font-bold tracking-tight md:text-lg">{title}</p>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </motion.a>
   );
 }

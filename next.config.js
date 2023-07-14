117a229b702079a035bb2d622a7535dd260e691b
pySoo <soopy368@gmail.com>
2023-07-14 13:26:47 +0900

    [chore] : fonts loaders를 config 파일에 설정

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ const { withContentlayer } = require('next-contentlayer');
 module.exports = withContentlayer({
   reactStrictMode: true,
   swcMinify: false,
+  experimental: {
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    ],
+  },
 });

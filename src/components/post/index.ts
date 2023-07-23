7be8b69c267f6834ff859d1ec559e1df8a0b456d
pySoo <soopy368@gmail.com>
2023-07-23 23:03:57 +0900

    [refactor] : import 구문을 줄이기 위해 components 폴더에 index 파일 추가

---
+++ src/components/post/index.ts
@@ -0,0 +1,4 @@
+export { default as PostCard } from './PostCard';
+export { default as PostFooter } from './PostFooter';
+export { default as PostList } from './PostList';
+export { default as PostNavigation } from './PostNavigation';

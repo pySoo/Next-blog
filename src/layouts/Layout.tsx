ae80cfe0e8a0ced9baeaf1344df6c8f2e1a02c2a
pySoo <soopy368@gmail.com>
2023-08-17 00:29:23 +0900

    [feat] : title 필수조건 제거

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,7 +6,7 @@ import SiteFooter from '@/components/common/SiteFooter';
 import { fadeInHalf, staggerHalf } from '@/constants/animations';
 
 type LayoutProps = {
-  title: string;
+  title?: string;
   description?: string | ReactNode;
   children: ReactNode;
 };
@@ -16,7 +16,7 @@ export default function Layout({ title, description, children }: LayoutProps) {
     <div className="mx-auto max-w-3xl px-6 lg:max-w-6xl lg:px-8">
       <HeaderNavigation />
       <main className="relative pb-16">
-        <Title>{title}</Title>
+        {title && <Title>{title}</Title>}
         <AnimatedContainer variants={staggerHalf} useTransition>
           {description && (
             <AnimatedContainer variants={fadeInHalf}>

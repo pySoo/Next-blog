f217c2de095b8f15105251b10d7b1e2d709b6c8b
pySoo <soopy368@gmail.com>
2023-08-18 15:07:05 +0900

    [feat] : 소개글 수정

--- src/components/home/IntroduceDescription.tsx
+++ src/components/home/IntroduceDescription.tsx
@@ -1,24 +1,14 @@
-import { AnimatedContainer, Pill } from '../common';
+import { Pill } from '../common';
 
 export default function IntroduceDescription() {
   return (
-    <div className="pt-2 flex flex-col sm:flex-row justify-between text-tertiary">
+    <div className="pt-2 flex flex-col justify-between text-tertiary">
       <p>
         반갑습니다 🤗 <br />
         어려운 것을 쉽게 설명하고 싶은 프론트엔드 개발자 박수현입니다.
         <br />
       </p>
-      <span className="pt-[15px] sm:hidden">
-        상단의{' '}
-        <AnimatedContainer
-          className="text-orange-500 font-bold"
-          whileTap={{ fontSize: '20px' }}
-        >
-          해바라기
-        </AnimatedContainer>
-        를 눌러서 블로그를 구경해 보시죠!
-      </span>
-      <div className="w-full flex flex-wrap items-center">
+      <div className="w-full flex flex-wrap items-center pt-3">
         <span className="mr-1">더 많은 글을 읽어보시려면</span>
         <a href={`https://velog.io/@soopy368`} target="_blank" rel="noreferrer">
           <Pill className="w-fit mr-1">Velog</Pill>

ebbebdf534a8528b5b462fa31fac98fcfc52590d
pySoo <soopy368@gmail.com>
2023-09-24 21:57:22 +0900

    [feat] : velog 소개글 제거

--- src/components/home/IntroduceDescription.tsx
+++ src/components/home/IntroduceDescription.tsx
@@ -1,5 +1,3 @@
-import { Pill } from '../common';
-
 export default function IntroduceDescription() {
   return (
     <div className="pt-2 flex flex-col justify-between text-tertiary">
@@ -8,13 +6,6 @@ export default function IntroduceDescription() {
         어려운 것을 쉽게 설명하고 싶은 프론트엔드 개발자 박수현입니다.
         <br />
       </p>
-      <div className="w-full flex flex-wrap items-center pt-3">
-        <span className="mr-1">더 많은 글을 읽어보시려면</span>
-        <a href={`https://velog.io/@soopy368`} target="_blank" rel="noreferrer">
-          <Pill className="w-fit mr-1">Velog</Pill>
-        </a>
-        <span>를 방문해주세요!</span>
-      </div>
     </div>
   );
 }
